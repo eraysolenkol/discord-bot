453b3cf31ebdac51dd7ee4ada6e9ceae64bdda67
GoHoleng <95094747+GoHoleng@users.noreply.github.com>
2022-03-20 01:12:11 +0000

    Delete dene.py

--- dene.py
+++
@@ -1,9 +0,0 @@
-from email.quoprimime import header_decode
-import json , requests , random 
-
-next_ev='Ivysaur'
-r3 = f'https://some-random-api.ml/pokedex?pokemon={next_ev}'
-data = json.loads(r3.text)
-pokeid = int(data['id'])
-img = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokeid}.png'
-print(pokeid)
